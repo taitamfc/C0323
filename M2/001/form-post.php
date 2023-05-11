5b4d52682f594b89d6244d2b7e239618eb5f3fa1
taitamfc <taitam8590@gmail.com>
2023-05-11 11:13:39 +0700

    add learn website

--- M2/001/form-post.php
+++ M2/001/form-post.php
@@ -11,10 +11,20 @@
 
    
     echo '<pre>';
-    print_r($_GET);
+    print_r($_REQUEST);
     echo '</pre>';
+
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+
+    if( $username == 'admin' && $password == '12345' ){
+        echo 'Xin chao admin';
+    }else{
+        echo 'Sai thong tin dang nhap';
+    }
+    
 ?>
-<form action="" method="GET">
+<form action="" method="POST">
     Username: <input type="text" name="username"> <br>
     Password: <input type="text" name="password"> <br>
     <input type="submit" value="Login">
