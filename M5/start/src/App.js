d20ccd8c3ea0a910202d7c326ac60eb466d9f6b9
taifamfc <taitam8590@gmail.com>
2023-08-31 08:37:34 +0700

    khai bao route

--- M5/start/src/App.js
+++ M5/start/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductIndex from "./pages/products/Index";
+import ProductCreate from "./pages/products/Create";
+import ProductEdit from "./pages/products/Edit";
+import ProductShow from "./pages/products/Show";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductIndex />} />
+        <Route path="/create" element={<ProductCreate />} />
+        <Route path="/edit" element={<ProductEdit />} />
+        <Route path="/show" element={<ProductShow />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
